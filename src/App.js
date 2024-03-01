60355f73563c2d73cffd52ae042182ee45e25d06
Gowtham <gowtham28044@gmail.com>
2024-03-01 20:58:37 +0530

    stoptwebcam done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import './App.css';
+import stopWebcam from './stopwebcam';
 
 function App() {
   const [capturedImage, setCapturedImage] = useState(null);
+  const [mediaStream, setMediaStream] = useState(null);
 
   const startWebcam = () => {
     navigator.mediaDevices.getUserMedia({ video: true })
       .then((stream) => {
         document.getElementById('video').srcObject = stream;
+        setMediaStream(stream);
       })
       .catch((error) => {
         console.log("Error accessing webcam:", error);
       });
   }
+
+  
 
   const capturePhoto = () => {
     const canvas = document.createElement('canvas');
@@ -20,8 +25,8 @@ function App() {
     canvas.height = 480;
     const context = canvas.getContext('2d');
     context.drawImage(document.getElementById('video'), 0, 0, 640, 480);
-
-    setCapturedImage(canvas.toDataURL('image/png'))
+    setCapturedImage(canvas.toDataURL('image/png'));
+    stopWebcam(mediaStream, setMediaStream);
   }
 
   return (
@@ -29,7 +34,9 @@ function App() {
       <h1>Webcam Capture</h1>
       <div className="container">
         <video id="video" autoPlay />
-        {capturedImage && <img src={capturedImage} alt="Captured" />}
+      
+        {capturedImage && <img src={capturedImage} alt="Captured" className="capturedimage" />}
+        
       </div>
       <div>
         <button onClick={startWebcam}>Start Camera</button>
